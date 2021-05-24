94e95c096324b653c84e215b3325d2b5d7d62a87
Nishant Sapkota <60746512+thenishantsapkota@users.noreply.github.com>
2021-05-24 23:01:05 +0545

    Update voice.py

--- cogs/voice.py
+++ cogs/voice.py
@@ -4,7 +4,6 @@ from discord.ext import commands
 from discord.ext.commands import check
 from discord import FFmpegPCMAudio
 from discord.utils import get
-import youtube_dl
 import os
 
 class Voice(commands.Cog):
